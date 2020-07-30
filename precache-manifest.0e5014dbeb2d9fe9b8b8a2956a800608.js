98d3c18d28d6fdc6635613b42a9976a40b926a6e
tasharry <kisoidkisoid@gmail.com>
2020-07-31 02:18:34 +0300

    Updates

---
+++ precache-manifest.0e5014dbeb2d9fe9b8b8a2956a800608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea995607651319f6ff882a344d9d8a6",
+    "url": "/starwars.github.io/index.html"
+  },
+  {
+    "revision": "805e0e36edaf87488e86",
+    "url": "/starwars.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee5d206e1be73d4afea1",
+    "url": "/starwars.github.io/static/js/2.8ce1cad9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwars.github.io/static/js/2.8ce1cad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "805e0e36edaf87488e86",
+    "url": "/starwars.github.io/static/js/main.775f44e0.chunk.js"
+  },
+  {
+    "revision": "67a26d9d92ebefd965ef",
+    "url": "/starwars.github.io/static/js/runtime-main.a551c0af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starwars.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
